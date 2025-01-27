7604cb7123361e0c8a38e036f01bee8e370bf22c
ArpanSankesh <arpansankesh830@gmail.com>
2025-01-27 12:19:25 +0530

    feture

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ const cookieParser = require('cookie-parser')
 
 const app = express();
 const userRouter = require('./routes/user.routes');
+const indexRouter = require('./routes/index.routes');
 
 app.set('view engine', 'ejs');
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
+app.use('/', indexRouter)
 app.use('/user', userRouter)
 
 app.listen('5050', () => {
